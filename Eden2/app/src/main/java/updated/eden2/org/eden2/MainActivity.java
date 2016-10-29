0201a3b9e6cd3594364a5c8cb12cc6d71b88631d
Rabbanir <rnr254@nyu.edu>
2016-10-29 00:33:36 -0400

    audio attempts

--- Eden2/app/src/main/java/updated/eden2/org/eden2/MainActivity.java
+++ Eden2/app/src/main/java/updated/eden2/org/eden2/MainActivity.java
@@ -2,16 +2,25 @@ package updated.eden2.org.eden2;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaRecorder recorder = null;
+    Button recording;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +28,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        recording = (Button) findViewById(R.id.profile_1);
 
+        recording.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        startRecording();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Log.v("UPPPPPPPPPP", "MCALLLLING STOP RECORDING!");
+                        stopRecording();
+                        break;
+                }
+                return false;
+            }
+        });
 
     }
+
+    private void startRecording(){
+        Log.v("STARTED RECORDING", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(getFilename());
+        recorder.setOnErrorListener(errorListener);
+        recorder.setOnInfoListener(infoListener);
+
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IllegalStateException e) {
+            Log.v("RECORDING ERROR", "THIS IS BAD");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private MediaRecorder.OnErrorListener errorListener = new MediaRecorder.OnErrorListener() {
+        @Override
+        public void onError(MediaRecorder mr, int what, int extra) {
+        }
+    };
+
+    private MediaRecorder.OnInfoListener infoListener = new MediaRecorder.OnInfoListener() {
+        @Override
+        public void onInfo(MediaRecorder mr, int what, int extra) {
+        }
+    };
+
+    private void stopRecording(){
+        if(null != recorder){
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+
+            recorder = null;
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,6 +133,12 @@ public class MainActivity extends AppCompatActivity {
         alertDialogBuilder.setMessage("This is a standard message");
         AlertDialog popUp = alertDialogBuilder.create();
         popUp.show();
+    }
+
+    public void record(View v) {
+        String mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+
     }
 
 }
