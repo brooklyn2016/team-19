ec370ad872652d13896c4f60ebf133d2264cc248
HeartlessFInale <heartlessfinale@hotmail.com>
2016-10-29 07:45:52 -0400

    Add files via upload
    Attempt to fix android no value bug

--- addperson.php
+++ addperson.php
@@ -1,12 +1,12 @@
 <?php
 	//This is blah
 	
-	//HTTPS Check
-	if($_SERVER['SERVER_PORT'] !== 443 && (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off')) 
+	//HTTPS Check, Seems to break android
+	/*if($_SERVER['SERVER_PORT'] !== 443 && (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off')) 
 	{
 		header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 		exit;
-	}
+	}*/
 	
 	//Database Start
 	session_start();
