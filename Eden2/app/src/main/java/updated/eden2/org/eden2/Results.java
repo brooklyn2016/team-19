f7683f3a19d51f97c124cfdf7af28077e9c19032
gbeargary <gc1753@nyu.edu>
2016-10-29 08:40:00 -0400

    See if textview works

--- Eden2/app/src/main/java/updated/eden2/org/eden2/Results.java
+++ Eden2/app/src/main/java/updated/eden2/org/eden2/Results.java
@@ -4,26 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Results extends AppCompatActivity {
+
+    public String toEdit = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        String toEdit = intent.getStringExtra("translation");
+        toEdit = intent.getStringExtra("translation");
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setPadding(30, 0, 30, 0);
         String[] texts = toEdit.split(" ");
         for(int i = 0; i < texts.length; i++) {
             TextView textView = new TextView(this);
             textView.setHint(texts[i]);
+            layout.addView(textView);
         }
         setContentView(R.layout.activity_results);
     }
 
     protected void goToEditAct(View v) {
         Intent intent = new Intent(this,EditActivity.class);
-        intent.putExtra("STRING_FROM_SERVER", "testString");
+        intent.putExtra("STRING_FROM_SERVER", toEdit);
         startActivity(intent);
     }
 }
