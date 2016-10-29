40548d0e0f893b232707fd905fbccc6cacaed69f
gbeargary <gc1753@nyu.edu>
2016-10-29 06:54:43 -0400

    Updated activities

--- Eden2/app/src/main/java/updated/eden2/org/eden2/EditActivity.java
+++ Eden2/app/src/main/java/updated/eden2/org/eden2/EditActivity.java
@@ -4,20 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class EditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String toEdit = intent.getStringExtra("STRING_FROM_SERVER");
+        String[] texts = toEdit.split(" ");
+        for(int i = 0; i < texts.length; i++) {
+            TextView textView = new TextView(this);
+            textView.setHint(texts[i]);
+        }
         setContentView(R.layout.activity_edit);
     }
 
     protected void goToResultsAct(View v) {
-        Intent intent = getIntent();
-        String toEdit = intent.getStringExtra("STRING_FROM_SERVER");
-        String[] texts = toEdit.split(" ");
-        //TODO: create texts.length number of textfields
         //TODO: post to server
         finish();
     }
